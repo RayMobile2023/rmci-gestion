9dc1cf2217ef0fce1e4c70bc3be472d75f188c88
RayMobile2023 <139542055+RayMobile2023@users.noreply.github.com>
2023-07-17 11:13:13 +0000

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -17,11 +17,7 @@ const config_db = {
   connectionLimit: 10,
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  ssl: {
-    ca: fs.readFileSync("../ssl/ca.pem"),
-    rejectUnauthorized: true,
-  },
+  queueLimit: 0
 };
 
 
